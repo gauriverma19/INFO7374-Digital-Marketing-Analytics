527ba18c2b0f00f8f9a5fb3ef1625f8d8fb2bb0e
gauriverma19 <65317465+gauriverma19@users.noreply.github.com>
2020-07-10 20:40:19 -0400

    assignment 3

--- Assignment 3/streamlit/streamlitApp.py
+++ Assignment 3/streamlit/streamlitApp.py
@@ -2,13 +2,62 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
+
+html = """
+  <style>
+    .reportview-container {
+      flex-direction: row-reverse;
+    }
+
+    header > .toolbar {
+      flex-direction: row-reverse;
+      left: 1rem;
+      right: auto;
+    }
+    
+    body {
+    color: #fff;
+    background-color: #09ab3b;
+    }
+
+    .sidebar .sidebar-collapse-control,
+    .sidebar.--collapsed .sidebar-collapse-control {
+      left: auto;
+      right: 0.5rem;
+      background-color: #09ab3b
+    }
+
+    .sidebar .sidebar-content {
+      transition: margin-right .3s, box-shadow .3s;
+      color: #fff;
+      background-color: #09ab3b;
+    }
+
+    .sidebar.--collapsed .sidebar-content {
+      margin-left: auto;
+      margin-right: -21rem;
+      background-color: #09ab3b;
+    }
+
+    @media (max-width: 991.98px) {
+      .sidebar .sidebar-content {
+        margin-left: auto;
+        background-color: #09ab3b
+      }
+    }
+  </style>
+"""
+
+st.markdown(html, unsafe_allow_html=True)
+
+
 add_selectbox = st.sidebar.radio(
     "Select a search method",
     ("Similarity Search", "Facebook AI")
-)
+    )
 
-if add_selectbox == 'Similarity Search' :
- st.title("**_Recommendations using similarity search_**")
+if add_selectbox == 'Similarity Search':
+ st.title("**_Recommendations using similarity search_** :camera:")
  st.write("-------------------------------------------------------------------------------------------------")
  def get_data():
      return pd.read_csv('algo1.csv')
@@ -34,7 +83,7 @@ if add_selectbox == 'Similarity Search' :
              n+=1
 
 elif add_selectbox == 'Facebook AI':
- st.title("Recommendations using facebook AI")
+ st.title("**_Recommendations using facebook AI_** :computer:")
  st.write("-------------------------------------------------------------------------------------------------")
  def get_data():
      return pd.read_csv('algo2.csv')
@@ -59,7 +108,7 @@ elif add_selectbox == 'Facebook AI':
              st.image(row[n], use_column_width=None, caption=row[n])
              n+=1
 
- 
+ st.balloons()
 
 
 
